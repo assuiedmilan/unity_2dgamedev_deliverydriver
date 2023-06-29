749b312f2d476e9b6b3d6ab15081845bfbb6ae7c
Milan Assuied <milan.assuied@unity3d.com>
2023-06-29 19:44:08 -0400

    Make driving independent from fixed update step

--- DeliveryDriver/Assets/Scripts/Driver/Driver.cs
+++ DeliveryDriver/Assets/Scripts/Driver/Driver.cs
@@ -3,56 +3,57 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    
     [SerializeField]
-    float rotationRate = 1f;
-    
+    float rotationRate = 250f;
+
     [SerializeField]
-    float translationRate = 0.01f;
-        
+    float translationRate = 20f;
+
     DriverInputs m_DriverInputs;
     DriverInputs.DrivingActions m_DrivingActions;
-    
+
     bool isMoving => m_DrivingActions.Moving.IsPressed();
     bool isSteering => m_DrivingActions.Steering.IsPressed();
+
+    float m_FixedUpdateStepTime;
     
     void Awake()
     {
         SetRefreshRate();
         RegisterInputsCallbacks();
+        
+        m_FixedUpdateStepTime = Time.fixedDeltaTime;
     }
 
     void FixedUpdate()
     {
         DoMove();
         DoSteer();
-         
     }
 
     void DoMove()
     {
-        if(isMoving)
+        if (isMoving)
         {
             var movingValue = m_DrivingActions.Moving.ReadValue<float>();
-            transform.Translate(0, movingValue * translationRate, 0);
+            transform.Translate(0, movingValue * translationRate * m_FixedUpdateStepTime, 0);
         }
     }
 
     void DoSteer()
     {
-       if(isSteering && isMoving)
-       {
-           var steeringValue = m_DrivingActions.Steering.ReadValue<float>();
-           transform.Rotate(0, 0, steeringValue * rotationRate);
-       }
-        
+        if (isSteering && isMoving)
+        {
+            var steeringValue = m_DrivingActions.Steering.ReadValue<float>();
+            transform.Rotate(0, 0, steeringValue * rotationRate * m_FixedUpdateStepTime);
+        }
     }
 
     static void SetRefreshRate()
     {
-        Time.fixedDeltaTime = 1/60f;
+        Time.fixedDeltaTime = 1 / 60f;
     }
-    
+
     void RegisterInputsCallbacks()
     {
         m_DriverInputs = new DriverInputs();
