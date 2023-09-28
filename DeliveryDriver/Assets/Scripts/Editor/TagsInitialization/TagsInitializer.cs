29942d77164e3e4ceb83102815d1c78d681909ca
Milan Assuied <milan.assuied@unity3d.com>
2023-09-28 10:21:22 -0400

    Extract reusable code

---
+++ DeliveryDriver/Assets/Scripts/Editor/TagsInitialization/TagsInitializer.cs
@@ -0,0 +1,23 @@
+﻿using CodeBox.Tools.Editor.Interfaces;
+using CodeBox.Tools.Editor.Tags;
+using Unity.DeliveryDriver.Editor.Enumerations;
+using UnityEditor;
+
+namespace Unity.DeliveryDriver.Editor.TagsInitialization
+{
+    public static class TagsInitializer
+    {
+        [InitializeOnLoad]
+        public static class Loader
+        {
+            class InternalEditorUtility : IInternalEditorUtility {}
+            static GameTagsManager<GameTags> s_Manager = new(new InternalEditorUtility());
+    
+            static Loader()
+            {
+                s_Manager.CleanTags();
+                s_Manager.InitializeTags();
+            }
+        }
+    }
+}
