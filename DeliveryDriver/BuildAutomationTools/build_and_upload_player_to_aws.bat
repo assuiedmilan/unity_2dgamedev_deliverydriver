7dbaabc487caf145056363c68d8aab54d1fb3729
Milan Assuied <milan.assuied@unity3d.com>
2023-10-17 13:24:45 -0400

    Figuring out relative paths

--- DeliveryDriver/BuildAutomationTools/build_and_upload_player_to_aws.bat
+++ DeliveryDriver/BuildAutomationTools/build_and_upload_player_to_aws.bat
@@ -4,15 +4,15 @@ echo Moving to automation tools folder from: %CD%
 cd DeliveryDriver\BuildAutomationTools 
 
 echo Building player from directory: %CD%
-call unity.exe --batchmode --quit --silent --projectPath .. --buildTarget Win64 --buildWindows64Player ../build/delivery_driver/delivery_driver.exe --logFile build_log
+call "C:\Program Files\Unity\Editor\Unity.exe" --batchmode --quit --silent --projectPath .. --buildTarget Win64 --buildWindows64Player ../build/delivery_driver/delivery_driver.exe --logFile build_log
 
 echo Creating upload folder from: %CD%
-mkdir build_upload
+mkdir ..\build_upload
 
 echo Calling zip from: %CD%
-call zip_folder_win.exe -s build/delivery_driver -n build_upload/delivery_driver -env BRANCH %SCM_BRANCH% REVISION %SCM_REVISION%
+call zip_folder_win.exe -s ..\build/delivery_driver -n build_upload/delivery_driver -env BRANCH %SCM_BRANCH%
 
 echo Calling upload from: %CD%
-call upload_to_aws_win -b thundr-staging -p build_upload -a delivery_driver\builds
+call upload_to_aws_win -b thundr-staging -p ../build_upload -a delivery_driver/builds
 
 cd..
--- DeliveryDriver/BuildAutomationTools/upload_assets_to_aws.bat
+++ DeliveryDriver/BuildAutomationTools/upload_assets_to_aws.bat
@@ -2,6 +2,6 @@ echo Moving to automation tools folder from: %CD%
 cd DeliveryDriver\BuildAutomationTools 
 
 echo Calling upload from: %CD%
-call upload_to_aws_win -b thundr-staging -p AssetBundles -a delivery_driver\asset-bundles
+call upload_to_aws_win -b thundr-staging -p ..\AssetBundles -a delivery_driver\asset-bundles
 
 cd..
