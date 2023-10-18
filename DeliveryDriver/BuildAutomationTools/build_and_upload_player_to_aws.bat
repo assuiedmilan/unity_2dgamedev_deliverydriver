432388af28c33b15e2b29edd3f7ae18cac594cca
Milan Assuied <milan.assuied@unity3d.com>
2023-10-18 07:14:42 -0400

    Try to have UCB archive the zip

--- DeliveryDriver/BuildAutomationTools/build_and_upload_player_to_aws.bat
+++ DeliveryDriver/BuildAutomationTools/build_and_upload_player_to_aws.bat
@@ -11,7 +11,10 @@ echo Creating upload folder from: %CD%
 mkdir ..\build_upload
 
 echo Calling zip from: %CD%
-call zip_folder_win.exe -s DeliveryDriver/build -d build_upload/delivery_driver -env SCM_BRANCH
+REM Zip must have the same name as the one expected by UCB to ensure it will be archived and appear as artifact. If artifacts are no longer uploaded, verify the UCB project name and find an old log to get/change the syntax
+REM Zipping twice as we want to upload a meaningful zipfile on AWS, which will contain the branch name
+call zip_folder_win.exe -s DeliveryDriver/build -d build_archive/deliverydriver
+call zip_folder_win.exe -s DeliveryDriver/build -d build_upload/deliverydriver -env SCM_BRANCH
 
 echo Calling upload from: %CD%
 call upload_to_aws_win -b thundr-staging -p build_upload -a delivery_driver/builds
