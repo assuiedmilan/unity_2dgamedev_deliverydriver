386061bc54cf2d0fe3d8e47a44c585117fc5b70a
Milan Assuied <milan.assuied@unity3d.com>
2023-10-19 07:52:25 -0400

    Trying to archive the build

--- DeliveryDriver/BuildAutomationTools/build_and_upload_player_to_aws.bat
+++ DeliveryDriver/BuildAutomationTools/build_and_upload_player_to_aws.bat
@@ -8,16 +8,14 @@ echo Moving to automation tools folder from: %CD%
 cd ..\BuildAutomationTools 
 
 echo Creating upload folder from: %CD%
-mkdir ..\build_upload
+set archive_target=../../build
+mkdir %archive_target%
 
 echo Calling zip from: %CD%
-REM Zip must have the same name as the one expected by UCB to ensure it will be archived and appear as artifact. If artifacts are no longer uploaded, verify the UCB project name and find an old log to get/change the syntax
-REM Zipping twice as we want to upload a meaningful zipfile on AWS, which will contain the branch name
-call zip_folder_win.exe -s DeliveryDriver/build -d build_archive/deliverydriver
-call zip_folder_win.exe -s DeliveryDriver/build -d build_upload/deliverydriver -env SCM_BRANCH
+call zip_folder_win.exe -s DeliveryDriver/build -d %archive_target%/deliverydriver -env SCM_BRANCH
 
 echo Calling upload from: %CD%
-call upload_to_aws_win -b thundr-staging -p build_upload -a delivery_driver/builds
+call upload_to_aws_win -b thundr-staging -p %archive_target% -a _milan_tests/delivery_driver/builds
 
 cd..
 cd..
